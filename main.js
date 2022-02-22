3e70a89634ae0afcedd8c5ea83af7ae18ae48927
xLiwiax <boliwia333@gmail.com>
2022-02-23 00:55:18 +0100

    zadanie13

---
+++ main.js
@@ -0,0 +1,7 @@
+function fun(n){
+    if (n>=100 && n<=200){
+        console.log('Liczba znajduje się w przedziale')
+    } else{
+        console.log('Liczba nie znajduje się w przedziale')
+    }
+}
