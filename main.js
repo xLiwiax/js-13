6dffec0cac253bb76a22e5335afb49e06ac0945e
xLiwiax <boliwia333@gmail.com>
2022-02-24 12:50:36 +0100

    zadanie 13

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 function fun(n){
-    if (n>=100 && n<=200){
+    if ((n >= 100) && (n <= 200)){
         console.log('Liczba znajduje się w przedziale')
     } else{
         console.log('Liczba nie znajduje się w przedziale')
     }
 }
+fun(160)
